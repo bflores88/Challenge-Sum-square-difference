722a4f3e51497e536331a8efff4ef1d93fd5d8f0
kmietotwy <kmietotwy@gmail.com>
2019-04-08 09:48:58 -1000

    challenge 6

--- challenge_6.js
+++ challenge_6.js
@@ -6,11 +6,32 @@
  */
 exports.SumOfASquare = SumOfASquare;
 
-function SumOfASquare(naturalNumbers){
+function SumOfASquare(naturalNumbers) {
+
   this.naturalNumbers = naturalNumbers;
+
+  this.sumOfSquares = function () {
+    let sumSquares = 0;
+    for (let i = 1; i < this.naturalNumbers + 1; i++) {
+      sumSquares += Math.pow(i, 2);
+    }
+    return sumSquares;
+  };
+
+  this.squareOfTheSums = function () {
+    let sum = 0;
+    for (let i = 1; i < this.naturalNumbers + 1; i++) {
+      sum += i;
+    }
+    return Math.pow(sum, 2);
+  }
 }
 
+
 // do work here
+
+
+
 
 // overwrite the object prototype, or add methods to the prototype
 // to satisfy the two method calls, sumOfSquares() and squareOfTheSums()
